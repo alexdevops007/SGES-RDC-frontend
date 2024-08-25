e736ad12ab6baee9ae7eb6e6fbe5c9bdfd0fbc8b
Alex Simisi Nta <smsxander@gmail.com>
2024-08-26 00:46:04 +0200

    update files

--- src/components/reports/ReportDetail.vue
+++ src/components/reports/ReportDetail.vue
@@ -3,9 +3,7 @@
     <h2 class="text-2xl font-bold mb-4">{{ report.type }} - Détails</h2>
     <p>Généré par: {{ report.generatedBy.name }}</p>
     <p>Date: {{ formattedDate(report.createdAt) }}</p>
-    <ReportLineChart :reportData="report.data" />
-    <ReportPieChart :reportData="report.data" />
-    <div class="mt-4">
+    <div class="mt-6 mb-6">
       <button
         @click="exportPDF"
         class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-full mr-4"
@@ -18,6 +16,10 @@
       >
         Exporter en Excel
       </button>
+    </div>
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
+      <ReportLineChart :reportData="report.data" />
+      <ReportPieChart :reportData="report.data" />
     </div>
   </div>
   <p v-else class="text-pnc-gray">Chargement du rapport...</p>
@@ -46,15 +48,46 @@ export default {
     },
     exportPDF() {
       const doc = new jsPDF();
+
+      // Ajouter le titre
       doc.text(`Rapport: ${this.report.type}`, 10, 10);
-      doc.autoTable({ html: "#my-table" });
-      doc.save("report.pdf");
+
+      // Ajouter les détails du rapport
+      doc.text(`Généré par: ${this.report.generatedBy.name}`, 10, 20);
+      doc.text(`Date: ${this.formattedDate(this.report.createdAt)}`, 10, 30);
+
+      // Ajouter les données dans un tableau
+      const tableColumn = ["Statut", "Nombre"];
+      const tableRows = [];
+
+      Object.keys(this.report.data.byStatus).forEach((key) => {
+        const rowData = [key, this.report.data.byStatus[key]];
+        tableRows.push(rowData);
+      });
+
+      // Générer le tableau dans le PDF
+      doc.autoTable({
+        head: [tableColumn],
+        body: tableRows,
+        startY: 40,
+      });
+
+      doc.save(`report_${this.report.type}.pdf`);
     },
     exportExcel() {
-      const ws = XLSX.utils.json_to_sheet(this.report.data);
+      const wsData = [
+        ["Statut", "Nombre"],
+        ...Object.entries(this.report.data.byStatus).map(([status, count]) => [
+          status,
+          count,
+        ]),
+      ];
+
+      const ws = XLSX.utils.aoa_to_sheet(wsData);
       const wb = XLSX.utils.book_new();
       XLSX.utils.book_append_sheet(wb, ws, "Report Data");
-      XLSX.writeFile(wb, "report.xlsx");
+
+      XLSX.writeFile(wb, `report_${this.report.type}.xlsx`);
     },
   },
   created() {
